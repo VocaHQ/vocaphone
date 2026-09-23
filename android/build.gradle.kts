20bd6e436ecd1469839d8a10f89f88ee08fc8bb3
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-09-23 19:38:54 +0530

    fix(android): constrain vulnerable Gradle build dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+// AGP 9.4.1 still requests vulnerable versions of these build-tool libraries.
+// Constrain its classpath without adding them to either app flavor's runtime.
+buildscript {
+    dependencies {
+        constraints {
+            classpath("org.bouncycastle:bcprov-jdk18on:1.85")
+            classpath("org.bouncycastle:bcpkix-jdk18on:1.85")
+            classpath("org.bitbucket.b_c:jose4j:0.9.6")
+            classpath("org.jdom:jdom2:2.0.6.1")
+            classpath("org.apache.commons:commons-lang3:3.18.0")
+            classpath("org.apache.httpcomponents:httpclient:4.5.14")
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
